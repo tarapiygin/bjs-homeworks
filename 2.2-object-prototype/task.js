10aec41986327b3b52b559f6bcdfa6f73855cefa
Vassilyev Denis <tarapiygin@gmail.com>
2020-04-03 16:58:13 +0300

    task 2.2 complete

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,32 @@
+"use strict"
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if(typeof animal === 'undefined'){
+        return null;
+    };
+    const animalSound = animal.sound
+    return animalSound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let roundedAverage = 0;
+    if (marks.length > 0) {
+        const averageMark = marks.reduce((sum, mark) => (sum + mark), 0) / marks.length;
+        roundedAverage = Math.round(averageMark);
+    };
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const currentDate = new Date;
+    const userDate = new Date(dateOfBirthday);
+    //Дата дня рождения в текущем году
+    const currentBirthday = new Date(currentDate.getFullYear(), userDate.getMonth(),userDate.getDate());
+    let userAge = currentDate.getFullYear() - userDate.getFullYear();
+
+    //Если ДР в этом году еще небыло, то отнимаем 1 полный год
+    if(currentDate < currentBirthday){
+        userAge = userAge - 1;
+    };
+    let verdict = (userAge >= 18);
+    return verdict;
 }
